194afb95d05fc73b86c797e88cbbc9fb8694f64b
alex <oscar.vazquez4430@alumnos.udg.mx>
2025-07-25 20:06:00 -0600

    eliminar los test antes de modificar el html

--- config/auth_check.php
+++ config/auth_check.php
@@ -3,14 +3,19 @@
 session_start();
 
 function checkAuthentication() {
+    /*
     if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         http_response_code(401);
         echo json_encode(['error' => 'Authentication required']);
         exit;
     }
+        */
+    return true;
 }
 
 function isAuthenticated() {
-    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+    //Remover esto para la seguridad
+    //return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+    return true;
 }
 ?>
