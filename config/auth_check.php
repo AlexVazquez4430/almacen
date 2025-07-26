23efbb2a7c2ef3a4e5588cb878b6905b049b4dae
alex <oscar.vazquez4430@alumnos.udg.mx>
2025-07-25 21:51:02 -0600

    Regrese seguridad falta minimo de stock

--- config/auth_check.php
+++ config/auth_check.php
@@ -3,19 +3,16 @@
 session_start();
 
 function checkAuthentication() {
-    /*
+
     if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         http_response_code(401);
         echo json_encode(['error' => 'Authentication required']);
         exit;
     }
-        */
-    return true;
+
 }
 
 function isAuthenticated() {
-    //Remover esto para la seguridad
-    //return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
-    return true;
+    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
 }
 ?>
